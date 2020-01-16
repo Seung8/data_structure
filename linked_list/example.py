9e3a81df20f532a1b55eb40d6b56f516291696f6
Seungyun Lee <lsy0914@naver.com>
2020-01-16 21:39:15 +0900

    Node 및 간단한 LinkedList 구현

---
+++ linked_list/example.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    """
+    self.add(): 맨 뒤에 아이템 추가
+    self.desc(): 모든 노드 아이템 출력
+    """
+    def __init__(self, data):
+        # 최초 첫 번째 노드 생성
+        self.head = Node(data)
+
+    def add(self, data):
+        node = self.head
+
+        # node.next 가 None 이면 다음 노드로 이동
+        while node.next:
+            node = node.next
+
+        # 새 노드 생성
+        node.next = Node(data)
+
+    def desc(self):
+        node = self.head
+
+        # 모든 node 순회하며 출력
+        while node:
+            print(node.data)
+            # node 출력 후 node 변수에 다음 노드 지정
+            node = node.next
+
+
+if __name__ == '__main__':
+    pass
