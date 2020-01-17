b93883019264428fc8b6747eda58ef8348a504e3
Seungyun Lee <lsy0914@naver.com>
2020-01-17 12:59:49 +0900

    LinkedList 삭제 함수 delete 추가

--- linked_list/example.py
+++ linked_list/example.py
@@ -12,7 +12,9 @@ class LinkedList:
     """
     self.add(): 맨 뒤에 아이템 추가
     self.desc(): 모든 노드 아이템 출력
+    self.delete(): 특정 노드 삭제
     """
+
     def __init__(self, data):
         # 최초 첫 번째 노드 생성
         self.head = Node(data)
@@ -32,10 +34,51 @@ class LinkedList:
 
         # 모든 node 순회하며 출력
         while node:
-            print(node.data)
+            print('data: {}'.format(node.data))
             # node 출력 후 node 변수에 다음 노드 지정
             node = node.next
 
+    def delete(self, data):
+        if not self.head:
+            print('제거할 데이터 없음')
+            return
 
+        # 맨 앞 노드(self.head)를 삭제하는 경우
+        if self.head.data == data:
+            temp = self.head
+            self.head = temp.next
+            del temp
+
+        # 중간 노드 혹은 마지막 노드를 삭제하는 경우
+        else:
+            node = self.head
+            # 마지막 노드까지 순회
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = temp.next
+                    del temp
+                    return
+                else:
+                    node = node.next
+
+
+# 테스트
 if __name__ == '__main__':
-    pass
+    print('## 연결 리스트 생성')
+    linked_list1 = LinkedList(0)
+    linked_list1.desc()
+
+    print('## 헤드 제거')
+    linked_list1.delete(0)
+    linked_list1.desc()
+
+    print('## 0부터 9까지 노드 생성 후 출력')
+    linked_list2 = LinkedList(0)
+    for i in range(1, 10):
+        linked_list2.add(i)
+    linked_list2.desc()
+
+    print('## 중간 노드 4 삭제 후 출력')
+    linked_list2.delete(4)
+    linked_list2.desc()
